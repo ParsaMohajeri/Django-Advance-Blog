a4a0448c6b3459ea31686bdc0488424cb866d9dd
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-06 00:27:27 +0330

    I made models.py ready to migrate

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,9 +1,53 @@
 from django.db import models
-from django.coontrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    """
+    custome user model manager where email is the unique identifier for
+    authentication instead of usernames.
+    """
+    def create_user(self, email, password,**extra_fields):
+        """
+        create and save a User with the given email and password
+        and or extra data
+        """
+        if not email:
+            raise ValueError(_("the email must be set"))
+        email= self.normalize_email(email)
+        user= self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+        
+
+
+    def create_superuser(self,email,password,**extra_fields):
+        """
+        create and save a User with the given email and password
+        """
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        extra_fields.setdefault('is_active',True)
+
+
+        if extra_fields.get ('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get ('is_superuser') is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))     
+
+        return self.create_user(email,password,**extra_fields)
+        
+
+
+
 class User (AbstractBaseUser, PermissionsMixin):
+    """
+    custome User model for our app
+    """
     email = models.EmailField(max_length=255,unique=True)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.BooleanField(default=False)
@@ -14,7 +58,7 @@ class User (AbstractBaseUser, PermissionsMixin):
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
 
-
+    objects = UserManager()
 
 
 
