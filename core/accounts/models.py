765af1c44f4b1cd3082446eef7739a3e0aa21074
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-05 22:33:47 +0330

    Added first codes for accounts app models.py

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.coontrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
 # Create your models here.
+
+class User (AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=20)
+
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELD =[]
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+
+
+
+
+
+    def __str__(self):
+        return self.email
+
+
