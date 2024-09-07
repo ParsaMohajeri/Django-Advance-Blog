45282b4619fbe7dc741323af3c32b4c290053eee
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-07 23:45:19 +0330

    Edited all admin view and made migrations

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Profile
 
 # Register your models here.
+
+
+
+
+class CustomeUserAdmin(UserAdmin):
+    model=User 
+    list_display =('email','is_superuser','is_active')
+    list_filter =('email','is_superuser','is_active')
+    searching_fields=('email',)
+    ordering=('email',)
+    fieldsets = [
+        ('Athentication', {"fields": ["email", "password"]}),
+        ("Permissions", {"fields": ["is_staff","is_active","is_superuser"]}),
+        ("group permissions", {"fields": ["groups","user_permissions"]}),
+        ("important date", {"fields": ["last_login"]}),
+    ]
+    add_fieldsets = [
+        (None,{"classes": ["wide"],"fields": ["email", "password1","password2", "is_staff", "is_active","is_superuser"],},),
+    ]
+admin.site.register(Profile)
+admin.site.register(User,CustomeUserAdmin)
