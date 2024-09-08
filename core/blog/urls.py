8ac85c950a25956aa178f31daf2b3bbea2c96946
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-09 02:11:15 +0330

    testing class based view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import indexView
+from . import views
+from django.views.generic import TemplateView
+
+
 urlpatterns = [
-    path('', indexView, name = "fbv-test"),
+    path('fbv-index/', views.indexView, name = "fbv-index"),
+    # path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"Parsa"})),
+    path('cbv-index/',views.IndexView.as_view(), name = "cbv-index"),
+
 ]
