8a833f276b4fb6b05b16e8c2a23b5d51e1481e2b
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-11 11:06:56 +0330

    learning redirect by coding

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
+app_name="blog"
 
 urlpatterns = [
-    path('fbv-index/', views.indexView, name = "fbv-index"),
+    # path('fbv-index/', views.indexView, name = "fbv-index"),
     # path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context={"name":"Parsa"})),
     path('cbv-index/',views.IndexView.as_view(), name = "cbv-index"),
-
+    path('go-to-index/',views.RedirectToMaktab.as_view(),name='redirect-to-maktab' )
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView,RedirectView
 from .models import Post
 # Create your views here.
+
+
+# function base view shows a Template
+"""
 def indexView(request):
-    """
-    a class based view to show index page
-    """
+
     name="parsa"
     context={"name":name}
     return render(request, "index.html",context)
-
+"""
 class IndexView(TemplateView):
     template_name="index.html"
     def get_context_data(self,**kwargs):
@@ -17,3 +19,13 @@ class IndexView(TemplateView):
         context["name"]="parsa"
         context["posts"]=Post.objects.all()
         return context
+
+
+"""FBV for redirect
+from django.shortcuts import redirect
+def RedirectToMaktab(request):
+    return redirect('https://maktabkhooneh.org/')
+"""
+class RedirectToMaktab(RedirectView):
+    url="http://maktabkhooneh.org"
+    
