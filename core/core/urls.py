fadb1a8352b8328ca3e13f87e17e3dcda5afb317
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-07-30 20:47:10 +0330

    Added API url to work

--- core/core/urls.py
+++ core/core/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('blog/', include('blog.urls')),
-
 ] 
 
 # serving static and media for development
