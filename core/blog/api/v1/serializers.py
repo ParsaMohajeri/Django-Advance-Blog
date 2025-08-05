df76ab8c6c381c7f65482a06d0cd1f106e7143cd
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-08-05 23:05:01 +0330

    Update serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -6,17 +6,48 @@ from ...models import Post,Category
 #     id=serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
 
-class PostSerializer (serializers.ModelSerializer):
-
-    class Meta:
-        model =Post
-        fields = ['id','title','content','status','created_date','published_date','category','author']
-
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Category
         fields =['id','name']
+
+class PostSerializer (serializers.ModelSerializer):
+    snippet=serializers.ReadOnlyField(source='get_snippet')
+    relative_url=serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    class Meta:
+        model =Post
+        fields = ['id','title','image','content','author','status','category','snippet','relative_url','absolute_url','created_date','published_date']
+
+
+
+
+    def get_abs_url(self,obj):
+        request =self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
+
+
+
+
+
+
+
+    def to_representation(self, instance):
+        request =self.context.get("request")
+        rep= super().to_representation(instance)
+        rep['state']='list'
+        if request.parser_context.get('kwargs').get('pk'):
+            rep['state']='single'
+            rep.pop('snippet',None)
+            rep.pop('relative_url',None)
+            rep.pop('absolute_url',None)
+        else:
+            rep.pop('content',None)
+
+        rep['category']= CategorySerializer(instance.category,context={'request':request}).data
+        return rep
 
 
 
