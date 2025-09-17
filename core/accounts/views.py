6ad2325b2654a5f5084e66f88d38a74cf7ebf654
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-09-17 23:53:14 +0330

    Added cashing

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from time import sleep
 from .tasks import sendEmail
-
+import requests
 # __________________________
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1> Done Sending</h1>")
+
+@cache_page(60)
+def test(request):
+    response =requests.get("https://e2805c1f-8652-4d18-b5b2-3b0d732bbd34.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
--- core/core/settings.py
+++ core/core/settings.py
@@ -187,4 +187,15 @@ EMAIL_PORT = 25
 CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 
+# cashing configs
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
